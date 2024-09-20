17b5d0569d144c2284446327d0c56081739c1f18
RuralNative <johnberlinleonor@yahoo.com>
2024-09-21 02:15:33 +0800

    Created Function for Operator Pad Creation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 let createNumButtons = () => {
     for (i = 0; i < 10; i++) {
         let numButton = document.createElement("button")
-        numButton.classList.add('button', `number-${i}`)
+        numButton.classList.add('num-button', `number-${i}`)
         numButton.textContent = `${i}`
-        let parent = document.querySelector('num-buttons').appendChild(numButton)
+        document.querySelector('num-buttons').appendChild(numButton)
+    }
+}
+
+let createOperatorsButtons = () => {
+    const operators = ['+', '-', '*', '/', '=']
+    for (i = 0; i < 5; i++) {
+        let operatorButton = document.createElement("button")
+        operatorButton.classList.add('operator-button', `operator-${operators[0]}`)
+        operatorButton.textContent = `${operators.shift}`
+        document.querySelector('operator-buttons').appendChild(operatorButton)
     }
 }
